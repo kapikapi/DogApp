4202c6d059c9c09d7c149c16f4a82b132fdaa41f
Elizabeth Kapitonova <hellokapikapi@gmail.com>
2016-08-01 01:09:33 +0300

    Message is shown at the main page

--- src/main/java/com/epam/dog/MainServlet.java
+++ src/main/java/com/epam/dog/MainServlet.java
@@ -8,21 +8,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-//        @Override
-//        protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-//                throws ServletException, IOException {
-//
-//        }
+        response.setContentType("text/html");
 
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            response.setContentType("text/html");
-
-            PrintWriter out = response.getWriter();
-            out.println("<h1>Hello, dog world!</h1>");
-        }
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, dog world!</h1>");
+    }
 
 
 }
