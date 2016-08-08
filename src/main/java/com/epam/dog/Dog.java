fc2cea181aef476c36f68d9f82b60ce40a52a604
Elizabeth Kapitonova <hellokapikapi@gmail.com>
2016-08-08 09:32:36 +0300

    Spring mvc: update, add and show dog methods

---
+++ src/main/java/com/epam/dog/Dog.java
@@ -0,0 +1,45 @@
+package com.epam.dog;
+
+class Dog {
+
+    private int id;
+    private String name;
+    private int height;
+    private int weight;
+
+    Dog(int id, String name, int height, int weight) {
+        this.id = id;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
