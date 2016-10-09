e9c1d01ff22073b9b1fe335bdcc160e4a90763a3
Elizabeth Kapitonova <hellokapikapi@gmail.com>
2016-10-10 00:47:54 +0300

    JDK Proxies used directly without Spring

---
+++ src/main/java/com/epam/dog/DogDaoLogger.java
@@ -0,0 +1,24 @@
+package com.epam.dog;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class DogDaoLogger implements InvocationHandler{
+
+    private Object invocationTarget;
+
+    public DogDaoLogger(Object invocationTarget) {
+        this.invocationTarget = invocationTarget;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        System.out.println("Before execution");
+        Object result = method.invoke(invocationTarget, args);
+        System.out.println("After execution");
+
+        return result;
+    }
+}
