8ba7d5038e233a0f707c6276624b867dce45c46b
Elizaveta_Kapitonova <Elizaveta_Kapitonova@epam.com>
2016-10-19 15:48:29 +0300

    Validation test checks validator

--- src/test/java/com/epam/dog/dao/ValidationTest.java
+++ src/test/java/com/epam/dog/dao/ValidationTest.java
@@ -1,6 +1,7 @@
 package com.epam.dog.dao;
 
 import com.epam.dog.vo.Dog;
+import com.epam.dog.vo.DogDto;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,13 +17,7 @@ import static org.testng.Assert.assertEquals;
 
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml"})
 public class ValidationTest extends AbstractTransactionalTestNGSpringContextTests{
-
-    @Autowired
-    private DogDAO dogDAO;
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
+    
     private static Validator validator;
 
     @BeforeClass
@@ -31,12 +26,21 @@ public class ValidationTest extends AbstractTransactionalTestNGSpringContextTest
         validator = factory.getValidator();
     }
 
+    private Dog createDog(int id, String name, int height, int weight) {
+        Dog dog = new Dog();
+        dog.setId(id);
+        dog.setName(name);
+        dog.setHeight(height);
+        dog.setWeight(weight);
+        return dog;
+    }
+
     @Transactional
-    @Test(expectedExceptions = ConstraintViolationException.class)
-    public void checkNameConstraintsInSave() {
-        Dog savedDog = dogDAO.saveDog("A", 40, 10);
+    @Test
+    public void checkNameSizeConstraint() {
+        Dog dog = createDog(1, "A", 40, 10);
         Set<ConstraintViolation<Dog>> constraintViolations =
-                validator.validate(savedDog);
+                validator.validate(dog);
 
         assertEquals( 1, constraintViolations.size() );
         assertEquals("size must be between 2 and 200",
@@ -46,14 +50,13 @@ public class ValidationTest extends AbstractTransactionalTestNGSpringContextTest
     }
 
     @Transactional
-    @Test(expectedExceptions = ConstraintViolationException.class)
-    public void checkNameConstraintsInUpdate() {
-        Dog savedDog = dogDAO.saveDog("B", 30, 20);
+    @Test
+    public void checkHeightWeightSizeConstraint() {
+        Dog dog = createDog(2, "Bbbb", 40, 40);
         Set<ConstraintViolation<Dog>> constraintViolations =
-                validator.validate(savedDog);
-
+                validator.validate(dog);
         assertEquals(1, constraintViolations.size());
-        assertEquals("size must be between 2 and 200",
+        assertEquals("Height and weight are not balanced!",
                 constraintViolations.iterator().next().getMessage()
         );
     }
